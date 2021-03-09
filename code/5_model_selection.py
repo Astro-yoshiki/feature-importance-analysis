04eee99463ff980d13f0bfe96574b117b56c312a
Astro-yoshiki <white0107rose@gmail.com>
2021-03-09 17:02:27 +0900

    コードのバグを修正

--- code/5_model_selection.py
+++ code/5_model_selection.py
@@ -27,11 +27,10 @@ plt.rcParams["grid.linewidth"] = 0.3
 
 
 class ModelSelection:
-    def __init__(self, path_, kernel_=None) -> None:
+    def __init__(self, path_) -> None:
         """
         前処理を事前に行う. ここではStandardScalerを用いた標準化をしている
         @type path_: string
-        @type kernel_: object
         """
         df = pd.read_csv(path_)
         self.x = df.iloc[:, :-1].values
@@ -44,11 +43,6 @@ class ModelSelection:
         self.x_test_std = self.x_sc.transform(self.x_test)
         self.y_train_std = self.y_sc.fit_transform(self.y_train)
         self.y_test_std = self.y_sc.transform(self.y_test)
-
-        self.gp = GaussianProcessRegressor(kernel=kernel_, n_restarts_optimizer=30)
-        # FIXME: ここでインスタンス変数にすると, 「"XGBRegressor" object is not callable :152」と表示される...
-        # self.gbdt = xgb.XGBRegressor()
-        # self.ga2m = ExplainableBoostingRegressor()
 
     @staticmethod
     def rmse_score(y_true: np.array, y_pred: np.array) -> float:
@@ -63,103 +57,100 @@ class ModelSelection:
         score_funcs = {
             "rmse": make_scorer(self.rmse_score, greater_is_better=False)
         }
-
         scores = cross_validate(model_, self.x_train_std, self.y_train_std, cv=kf, scoring=score_funcs)
         mean_rmse = scores["test_rmse"].mean()
         return mean_rmse * (-1)
 
-    def gaussian_process(self) -> object:
+    def gaussian_process(self, kernel_=None) -> object:
         """ガウス過程回帰を行う関数 """
-        result_ = self.cross_validation(model_=self.gp)
-        return result_, self.gp
+        gp_ = GaussianProcessRegressor(kernel=kernel_, n_restarts_optimizer=30)
+        result_ = self.cross_validation(model_=gp_)
+        gp_.fit(self.x_train_std, self.y_train_std)
+        return result_, gp_
 
     def gbdt(self) -> object:
         """勾配ブースティングによる回帰を行う関数"""
         gbdt_ = xgb.XGBRegressor()
         result_ = self.cross_validation(model_=gbdt_)
-        return result_, self.gbdt
+        gbdt_.fit(self.x_train_std, self.y_train_std)
+        return result_, gbdt_
 
     def ga2m(self) -> object:
         """一般化加法モデルによる回帰を行う関数"""
         ga2m_ = ExplainableBoostingRegressor()
         result_ = self.cross_validation(model_=ga2m_)
-        return result_, self.ga2m
+        ga2m_.fit(self.x_train_std, self.y_train_std)
+        return result_, ga2m_
 
     def visualization(self, model_, method=None) -> None:
         """予測結果の可視化を行う関数"""
-        # ガウス過程回帰の場合は分散も可視化するため, 処理を分けている
-        if method == "GPR":
-            y_pred_std, y_var_std = model_.predict(self.x_test_std, return_std=True)
-            y_pred = self.y_sc.inverse_transform(y_pred_std)
-            y_var = y_var_std * self.y_sc.scale_
-            y_std = y_var ** 0.5
-
-            fig, ax = plt.subplots(figsize=(5, 5))
-            ax.plot(self.y_test, y_pred, "bo")
-            ax.plot([min(self.y_test), max(self.y_test)], [min(self.y_test), max(self.y_test)], color="r")
-            ax.fill_between(self.y_test, y_pred.reshape(-1) - y_std, y_pred.reshape(-1) + y_std,
-                            alpha=0.3, color="steelblue", label="1σ")
-            ax.set_xlabel("Actual")
-            ax.set_ylabel("Prediction")
-            ax.set_title(method)
-            ax.legend(loc="best")
-        elif method == "Ensemble":
-            y_pred = np.zeros((len(model_), len(self.y)))
+        if method == "Ensemble":
+            y_train_pred = np.zeros((len(model_), len(self.y_train)))
+            y_test_pred = np.zeros((len(model_), len(self.y_test)))
             for i in range(len(model_)):
-                y_pred_std = model_[i].predict(self.x_test_std)
-                y_pred[i] = self.y_sc.inverse_transform(y_pred_std)
-            y_pred_mean = np.mean(y_pred, axis=0)
+                # 訓練データ
+                y_train_pred_std = model_[i].predict(self.x_train_std).reshape(-1)
+                y_train_pred[i] = self.y_sc.inverse_transform(y_train_pred_std)
+                # テストデータ
+                y_test_pred_std = model_[i].predict(self.x_test_std).reshape(-1)
+                y_test_pred[i] = self.y_sc.inverse_transform(y_test_pred_std)
+            y_train_pred_mean = np.mean(y_train_pred, axis=0)
+            y_test_pred_mean = np.mean(y_test_pred, axis=0)
 
             fig, ax = plt.subplots(figsize=(5, 5))
-            ax.plot(self.y_test, y_pred_mean, "bo")
-            ax.plot([min(self.y_test), max(self.y_test)], [min(self.y_test), max(self.y_test)], color="r")
+            ax.plot(self.y_train, y_train_pred_mean, "bo", label="Train")
+            ax.plot(self.y_test, y_test_pred_mean, "ro", label="Test")
+            ax.plot([min(self.y), max(self.y)], [min(self.y), max(self.y)], color="black", linestyle="dashed")
             ax.set_xlabel("Actual")
             ax.set_ylabel("Prediction")
             ax.set_title(method)
             ax.legend(loc="best")
         else:
-            y_pred_std = model_.predict(self.x_test_std)
-            y_pred = self.y_sc.inverse_transform(y_pred_std)
+            y_train_pred_std = model_.predict(self.x_train_std)
+            y_train_pred = self.y_sc.inverse_transform(y_train_pred_std)
+            y_test_pred_std = model_.predict(self.x_test_std)
+            y_test_pred = self.y_sc.inverse_transform(y_test_pred_std)
 
             fig, ax = plt.subplots(figsize=(5, 5))
-            ax.plot(self.y_test, y_pred, "bo")
-            ax.plot([min(self.y_test), max(self.y_test)], [min(self.y_test), max(self.y_test)], color="r")
+            ax.plot(self.y_train, y_train_pred, "bo", label="Train")
+            ax.plot(self.y_test, y_test_pred, "ro", label="Test")
+            ax.plot([min(self.y), max(self.y)], [min(self.y), max(self.y)], color="black", linestyle="dashed")
             ax.set_xlabel("Actual")
             ax.set_ylabel("Prediction")
             ax.set_title(method)
             ax.legend(loc="best")
 
-        # show plots
+        # save plots
         fig.tight_layout()
-        plt.savefig("../figure/prediction_{0}.png".format(str(method)), dpi=100)
-        plt.show()
+        plt.savefig("../figure/prediction_{}.png".format(str(method)), dpi=100)
+        plt.close()
 
 
 if __name__ == "__main__":
     # TODO: 変数選択したファイルの指定
-    path = "../data/processed_data/master_data_length.csv"
+    label = "length"
+    path = "../data/processed_data/model_selection_{}.csv".format(label)
 
     # ガウス過程回帰
     kernel = C(1.0, (1e-2, 1e2)) * RBF(1.0, (1e-2, 1e2)) + Wh(0.01, (1e-2, 1e2))
-    ms = ModelSelection(path, kernel_=kernel)
-    result, gp = ms.gaussian_process()
-    print("RMSE: ", result)
-    # FIXME: 以下のコードで, 「process finished with exit code 139 (interrupted by signal 11: sigsegv)」というエラーが出る
+    ms = ModelSelection(path)
+    result, gp = ms.gaussian_process(kernel_=kernel)
+    print("RMSE of GPR: ", result)
     ms.visualization(model_=gp, method="GPR")
 
     # 勾配ブースティング
-    ms = ModelSelection(path, kernel_=None)
+    ms = ModelSelection(path)
     result, gbdt = ms.gbdt()
-    print("RMSE: ", result)
+    print("RMSE of GBDT: ", result)
     ms.visualization(model_=gbdt, method="GBDT")
 
     # 一般化加法モデル
-    ms = ModelSelection(path, kernel_=None)
+    ms = ModelSelection(path)
     result, ga2m = ms.ga2m()
-    print("RMSE: ", result)
+    print("RMSE of GA2M: ", result)
     ms.visualization(model_=ga2m, method="GA2M")
 
     # アンサンブル
-    ms = ModelSelection(path, kernel_=None)
+    ms = ModelSelection(path)
     model = [gp, gbdt, ga2m]
     ms.visualization(model_=model, method="Ensemble")
